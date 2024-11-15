93c5b6b1e649cf504f5427d3b709dccc4ab6ebfc
Vik Ventor <130216079+VikVentor@users.noreply.github.com>
2024-11-15 07:47:33 +0530

    Update gem_medico.py

--- gem_medico.py
+++ gem_medico.py
@@ -5,7 +5,7 @@ import urllib.parse
 import webbrowser
 
 # Configure the Google Generative AI API
-genai.configure(api_key="AIzaSyAKi62cSIUc6fVt5XH0MZWN9G3WDkDCuCs")
+genai.configure(api_key="youkey")
 
 # Choose a Gemini model.
 model = genai.GenerativeModel(model_name="gemini-1.5-pro")
